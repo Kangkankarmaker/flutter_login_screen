aa6f332ca3e085515adda13237e2b4533ce564dc
kangkankarmaker <kangkankarmaker00@gmail.com>
2021-01-31 22:23:41 +0600

    Initial commit

---
+++ android/app/src/main/java/k/example/flutter_login_screen/MainActivity.java
@@ -0,0 +1,6 @@
+package k.example.flutter_login_screen;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const kBankgroundColor=Color(0xFF202020);
+const kPrimaryColor=Color(0xFFFFBD73);
